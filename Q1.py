e2b5338837d96a72e40dd7dd42a3d31208ea39b2
lot nikee <lotnikee@github.com>
2025-07-16 12:49:01 +0100

    commit

--- Q1.py
+++ Q1.py
@@ -1,61 +1,35 @@
+### Import useful packages
 import matplotlib.pyplot as plt 
 import numpy as np 
-import random
+import pandas as pd 
+import seaborn as sns; sns.set()
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 
-### Define a range of sampled x values
-x = np.linspace(0, 100, 200)
+### This section of the code generates the training data for fitting the Gaussian Process Regression model. 
+x_end = 2                                                                               ### End point of the training set interval 
+train_datapoints = 200                                                                  ### Number of extracted training set data points 
+X_n = np.linspace(start = 0, stop = x_end, num = train_datapoints)                        ### Generating the actual data points 
 
-### Define a simple function 
-y_true = np.sin(2 * x)
+### Define a simple function f(x)
+def f(X_n):
+    f = np.sin((2 * np.pi) * X_n)                                                         ### f(x) = sin(2πx)
+    return (f)
 
-### Generate noise with the same size as that of the sampled data
-noise = np.random.normal(0, 0.1, x.shape)
+### Store results in variable f_x
+f_x = f(X_n)
 
-### Generate the new function, including noise 
-y_noise = y_true + noise 
+### Adding noise to the functional evaluations 
+sigma_noise = 0.1                                                                       ### Sigma noise is the standard deviation of the normal distribution from which the random numbers are sampled
+epsilon_n = np.random.normal(loc = 0, scale = sigma_noise, size = train_datapoints)   ### Adding the noise to each training datapoint 
+y_n = f_x + epsilon_n                                                            ### y values of the training set is now the function + error in the training dataset
 
-### Plot the two functions to see how noise influences the datapoints
-fig, axs = plt.subplots(1,2, figsize=(12, 5))
-
-axs[0].plot(x, y_true)
-axs[0].set_title("Simple generated function")
-axs[0].set(xlabel = "x", ylabel = "y = sin(2x)")
-
-axs[1].plot(x, y_noise)
-axs[1].set_title("Simple generated function with noise")
-axs[1].set(xlabel = "x", ylabel = "y = sin(2x)")
-
+### Visualise the training data with a plot
+fig, ax = plt.subplots(figsize=(12, 5))
+sns.scatterplot(x = X_n, y = y_n, label = "training data", ax = ax);                  ### Scatterplot of the training set datapoints with added noise
+sns.lineplot(x = X_n, y = f_x, color = "red", label = "f(x)", ax = ax);                   ### Lineplot of the noise-free function
+ax.set(title ="Training data for GPR model")
+ax.legend(loc = "upper right")
+ax.set(xlabel = "x", ylabel = "y")
 plt.show()
 
-### Reshape x to fit 2D arrays 
-X = x.reshape(-1, 1)
-
-### Set up the kernels -> RBF for smoothness and WhiteKernel for noise 
-kernel  = RBF(length_scale=1.0) + WhiteKernel(noise_level=0.1)
-
-### Create and fit the GPR model 
-gpr = GaussianProcessRegressor(kernel=kernel)
-gpr.fit(X, y_noise)
-
-### Predict the data
-x_pred = np.linspace(0, 100, 2000).reshape(-1, 1)
-y_pred, y_std = gpr.predict(x_pred, return_std=True)
-
-### Plot results 
-plt.figure(figsize=(10, 5))
-plt.scatter(x, y_noise, color="red", label="Noisy Data")
-plt.plot(x, y_true, label="True function values", color="blue", linestyle="--")
-plt.plot(x_pred, y_pred, label="GPR mean", color="black")
-plt.fill_between(
-    x_pred.ravel(),
-    y_pred - 2*y_std, y_pred + 2*y_std,
-    alpha=0.2, color="grey", label="95% confidence interval"
-)
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("GPR fit to sin(2x) with noise")
-plt.legend()
-plt.tight_layout()
-plt.show()
