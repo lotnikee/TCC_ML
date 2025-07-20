b04b790cea894aa430e7eb2ebe56f232e7cbe5b9
lot nikee <lotnikee@github.com>
2025-07-20 14:54:07 +0100

    commit

--- QUESTION_4/Q4_a.py
+++ QUESTION_4/Q4_a.py
@@ -1,7 +1,42 @@
-import pandas as pd 
-from tqdm import tqdm
+import ase.db
+from dscribe.descriptors import SOAP
+import numpy as np 
+from sklearn.decomposition import KernelPCA
+import matplotlib.pyplot as plt
 
-df = pd.read_pickle("/Users/lotburgstra/Desktop/TCC_ML/Question_4/data.pckl.gz", compression = 'gzip')
+db = ase.db.connect("/Users/lotburgstra/Desktop/TCC_ML/QUESTION_4/ase.db")
 
-df.to_csv("training_data.csv", index = False)
+structures = []
+for row in db.select():
+    structures.append(row.toatoms())
 
+all_species = set()
+for atoms in structures: 
+    all_species.update(atoms.get_chemical_symbols())
+all_species = sorted(all_species)
+
+
+
+
+soap = SOAP(
+    species = ["Al", "Ca", "Mg"],
+    r_cut = 5.0,
+    n_max = 8, 
+    l_max = 6,
+    sparse = False
+)
+
+X = np.array([soap.create(s).mean(axis = 0) for s in structures])
+
+kpca = KernelPCA(n_components = 2, kernel = "linear")
+X_kpca = kpca.fit_transform(X)
+
+print(structures[0])
+print(soap.create(structures[0]).mean(axis=0))
+
+plt.figure(figsize = (6,5))
+plt.scatter(X_kpca[:, 0], X_kpca[:, 1], s= 10)
+plt.title("2D Structure Map via SOAP and Kernel PCA")
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.show()
---
+++ QUESTION_4/notes.py
@@ -0,0 +1,18 @@
+import ase.db
+from dscribe.descriptors import SOAP
+import numpy as np 
+from sklearn.decomposition import KernelPCA
+import matplotlib.pyplot as plt
+
+db = ase.db.connect("/Users/lotburgstra/Desktop/TCC_ML/QUESTION_4/ase.db")
+
+structures = []
+for row in db.select():
+    structures.append(row.toatoms())
+
+all_species = set()
+for atoms in structures: 
+    all_species.update(atoms.get_chemical_symbols())
+all_species = sorted(all_species)
+
+print("Species in my dataset:", all_species)
